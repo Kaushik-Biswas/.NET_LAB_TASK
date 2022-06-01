d0a2b938f725999985f271365d8424531127e3ab
Kaushik-Biswas <biswaskaushik.2020@gmail.com>
2022-06-01 20:38:31 +0600

    Lab Task 1

---
+++ Task1_CV/Task1_CV/Controllers/HomePageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1_CV.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult SmallProfile()
+        {
+            return View();
+        }
+        public ActionResult Qualification()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
